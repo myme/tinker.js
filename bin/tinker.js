9915bed78ff4e541af8771cd2a8d60d6a4d29f65
Martin Øinæs Myrseth <myrseth@gmail.com>
2013-05-24 21:07:01 +0200

    Switch to using less

--- bin/tinker.js
+++ bin/tinker.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 var express = require('express');
+var compiless = require('express-compiless');
 var path = require('path');
 
 var app = express();
@@ -11,6 +12,7 @@ app.set('views', path.join(rootDir, 'views'));
 app.set('view engine', 'jade');
 
 app.use(express.logger());
+app.use(compiless({ root: path.join(rootDir, 'src') }));
 app.use('/lib', express.static(path.join(rootDir, 'lib')));
 app.use('/src', express.static(path.join(rootDir, 'src')));
 
