ed2f7c538da7fcefc79c4592ee0ab0b06bc732f0
Martin Øinæs Myrseth <myrseth@gmail.com>
2013-05-24 20:52:13 +0200

    Add express

---
+++ bin/tinker.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+var express = require('express');
+var path = require('path');
+
+var app = express();
+var port = 3000;
+var rootDir = path.join(__dirname, '..');
+
+app.set('views', path.join(rootDir, 'views'));
+app.set('view engine', 'jade');
+
+app.use(express.logger());
+app.use('/lib', express.static(path.join(rootDir, 'lib')));
+app.use('/src', express.static(path.join(rootDir, 'src')));
+
+app.get('/', function (req, res) {
+  res.render('index', {});
+});
+
+app.listen(port);
+console.log('Listening on http://localhost:' + port);
