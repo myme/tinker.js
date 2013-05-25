147c88a46a4782ba26327d7dac4a4ebb967aeecd
Martin Øinæs Myrseth <myrseth@gmail.com>
2013-05-25 11:27:46 +0200

    Add optimist for option parsing

--- bin/tinker.js
+++ bin/tinker.js
@@ -1,11 +1,25 @@
 #!/usr/bin/env node
+/*jshint es5: true */
 
 var express = require('express');
 var compiless = require('express-compiless');
 var path = require('path');
 
+var optimist = require('optimist')
+  .usage('Usage $0 [files]')
+  .default('port', 3000)
+  .alias('p', 'port')
+  .alias('h', 'help')
+  .describe('port', 'Run server on designated port')
+  .describe('help', 'Show this help text');
+
+var argv = optimist.argv;
+if (argv.help) {
+  optimist.showHelp();
+  process.exit(1);
+}
+
 var app = express();
-var port = 3000;
 var rootDir = path.join(__dirname, '..');
 
 app.set('views', path.join(rootDir, 'views'));
@@ -21,5 +35,5 @@ app.get('/', function (req, res) {
   res.render('index', {});
 });
 
-app.listen(port);
-console.log('Listening on http://localhost:' + port);
+app.listen(argv.port);
+console.log('Listening on http://localhost:' + argv.port);
