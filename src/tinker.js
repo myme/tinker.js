9669ab06af171d8e52f20dfcdb6d7f2a02c67ccf
Martin Øinæs Myrseth <myrseth@gmail.com>
2013-05-28 23:43:23 +0200

    Move JavaScript runner to separate module

--- src/tinker.js
+++ src/tinker.js
@@ -1,7 +1,5 @@
-/* jshint evil:true */
-
 window.Tinker = window.Tinker || {};
-window.Tinker = (function (el, utils) {
+window.Tinker = (function (el, utils, JSRunner) {
 
   'use strict';
 
@@ -96,65 +94,6 @@ window.Tinker = (function (el, utils) {
     };
 
     return LogController;
-  }());
-
-
-  // JavaScript runner
-
-  var JSRunner = (function () {
-    var MockConsole = (function () {
-      var genLogger = function (severity) {
-        return function () {
-          var args = Array.prototype.slice.call(arguments);
-          this.output.push({
-            severity: severity,
-            message: args.join(' ')
-          });
-        };
-      };
-
-      var MockConsole = function () {
-        this.output = [];
-      };
-
-      MockConsole.prototype.log   = genLogger('log');
-      MockConsole.prototype.error = genLogger('error');
-
-      return MockConsole;
-    }());
-
-    var JSRunner = function (options) {
-      options = options || {};
-      this.win = options.window || window,
-      this.debug = options.debug;
-    };
-
-    JSRunner.prototype.evaluate = function (javascript) {
-      return this.win['eval'](javascript);
-    };
-
-    JSRunner.prototype.run = function (javascript) {
-      var mockConsole = new MockConsole();
-      var _console = this.win.console;
-      var evaled;
-
-      this.win.console = mockConsole;
-      try {
-        evaled = this.evaluate(javascript);
-      } catch (e) {
-        if (this.debug) {
-          _console.error('EVAL ERROR:', e);
-        }
-      }
-      this.win.console = _console;
-
-      return {
-        value: evaled,
-        logs: mockConsole.output
-      };
-    };
-
-    return JSRunner;
   }());
 
 
@@ -443,4 +382,4 @@ window.Tinker = (function (el, utils) {
     return Tinker;
   }());
 
-}(window.el, window.Tinker.utils));
+}(window.el, window.Tinker.utils, window.Tinker.JSRunner));
