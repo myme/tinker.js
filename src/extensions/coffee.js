5abd29e2691791889b85c1c40b63133ad76daefa
Martin Øinæs Myrseth <myrseth@gmail.com>
2013-06-05 01:10:55 +0200

    Execute CoffeScript and show compiled

--- src/extensions/coffee.js
+++ src/extensions/coffee.js
@@ -3,13 +3,34 @@
   'use strict';
 
   Tinker.addHandler('coffee', function (coffee) {
+    var output = [];
     var compiled;
+
     try {
-      compiled = el('pre', el('code', CoffeeScript.compile(coffee)));
+      compiled = CoffeeScript.compile(coffee, { bare: true });
     } catch (e) {
       compiled = '';
     }
-    this.outputView.setOutput(compiled);
+
+    var result = this.runJS(compiled);
+    var value = result.value;
+    var exec;
+
+    if (!value || value.nodeType !== 1) {
+      try {
+        value = JSON.stringify(value, 0, 2);
+      } catch (e) {}
+      value = el('pre', el('code', value || ''));
+    }
+
+    this.output(el('.coffee', [
+      el('h3', 'Result'),
+      el('.coffee-exec', value),
+      el('h3', 'Compiled'),
+      el('.coffee-compiled', el('pre', el('code', compiled)))
+    ]));
+
+    this.log(result.logs);
   });
 
 }(window.el, window.Tinker));
